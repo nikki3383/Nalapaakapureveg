a8e5e11c642f5a0d9ca2492b1b854e5be73c261c
Nikshep S R <nikshepsraj6@gmail.com>
2024-08-02 17:15:37 +0530

    mobile css

--- nalapaaka-pure-veg/src/App.js
+++ nalapaaka-pure-veg/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -20,8 +20,8 @@ function App() {
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
-          <Footer />
         </main>
+        <Footer />
       </div>
     </Router>
   );
--- nalapaaka-pure-veg/src/components/GoogleReviews.js
+++ nalapaaka-pure-veg/src/components/GoogleReviews.js
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 const GoogleReviews = ({ placeId }) => {
   const [reviews, setReviews] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchReviews = async () => {
-      try {
-        const response = await axios.get(
-          `https://maps.googleapis.com/maps/api/place/details/json`,
-          {
-            params: {
-              place_id: placeId,
-              key: "AIzaSyCCquOiELyhfclO0OnN2lMi2pNtZvh2ito",
-              fields: "reviews",
-            },
-          }
-        );
-        console.log("Google Reviews API Response:", response.data);
-        setReviews(response.data.result.reviews);
-      } catch (err) {
-        console.error("Google Reviews API Error:", err);
-        setError(err.message);
+    const fetchReviews = () => {
+      if (!window.google) {
+        console.error("Google Maps JavaScript API not loaded.");
+        return;
       }
+
+      const map = new window.google.maps.Map(document.createElement("div"));
+      const request = {
+        placeId: placeId,
+        fields: ["reviews"],
+      };
+
+      const service = new window.google.maps.places.PlacesService(map);
+      service.getDetails(request, (place, status) => {
+        if (
+          status === window.google.maps.places.PlacesServiceStatus.OK &&
+          place.reviews
+        ) {
+          setReviews(place.reviews);
+        } else {
+          console.error("Error fetching reviews:", status);
+          setError("Error fetching reviews");
+        }
+      });
     };
 
     fetchReviews();
